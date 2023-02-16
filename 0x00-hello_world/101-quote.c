f2b81885abe87d6f8503056810736a6dd92ce835
Reem-degais <reem.degais.33@gmail.com>
2023-02-16 15:08:56 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <string.h>
 #include <unistd.h>
 /**
  * main - main block
@@ -7,11 +5,9 @@
  */
 int main(void)
 {
-	int s = strlen("and that piece of art is useful\" - Dora Korpar,
-		       	2015-10-19\n");
 
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      s);
+
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
 	return (1);
 }
