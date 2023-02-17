b1e03dfdb37c50d0ff962abdf48a6ca91d4d2b3a
Reem-degais <reem.degais.33@gmail.com>
2023-02-17 14:17:50 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,10 @@
 /* more headers goes there */
 
 /**
- * main -main block
- * Rrturn:0
+ * main - main block
+ * Description: Get a random number and print the number
+ * and if it is positive, negative, or zero
+ * Return: 0
  */
 int main(void)
 {
