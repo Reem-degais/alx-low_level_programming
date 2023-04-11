c4fb39ac84f384ac26309b379f2c23c55bbd2885
Reem-degais <reem.degais.33@gmail.com>
2023-04-10 19:39:45 -0700

    -

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -17,6 +17,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
 	if (text_content != NULL)
 	{
 		for (l = 0; text_content[l];)
