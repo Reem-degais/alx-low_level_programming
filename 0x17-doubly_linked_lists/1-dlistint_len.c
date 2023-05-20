0ec64640d15452081b0f1996e0ff025e23a65641
Reem-degais <reem.degais.33@gmail.com>
2023-05-19 18:24:05 -0700

    -

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,5 +1,10 @@
 #include "lists.h"
 
+/**
+ * dlistint_len - return the number of element.
+ * @h: a pointer to list
+ * Return: nu,ber of element.
+ */
 size_t dlistint_len(const dlistint_t *h)
 {
 	const dlistint_t *node = h;
