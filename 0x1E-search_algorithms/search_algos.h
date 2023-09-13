2ead10716efe2e047cfc89e0922023d801f36fba
Reem-degais <reem.degais.33@gmail.com>
2023-09-12 18:05:35 -0700

    linear search algorithm

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef __SEARCH_ALGOS__H__
+#define __SEARCH_ALGOS__H__
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+
+#endif
